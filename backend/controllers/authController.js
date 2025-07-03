243ee41b359ac925826234d4660e12807b6602db
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-03 15:21:14 +0530

    feat: Add register user contoller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,9 +13,47 @@ exports.loginUser = async (req, res) => {
         if (!isMatch) return res.status(401).json({ message: "Invalid credentials"} );
 
         const token = generateToken(user._id, user.role);
-        res.json({ token, role: user.role });
-
+        res.json({ 
+            token, 
+            role: user.role,
+            approved: user.approved,
+            name: user.name
+        });
     } catch (err) {
         res.status(500).json({ message: "Server error" });
+    }
+}
+
+exports.registerUser = async (req, res) => {
+    const { name, email, password, role, institution, organizerRole, organization } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email, role });
+        if (existingUser) return res.status(400).json({ message: "Email already in use" });
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        let approved = false;
+
+        if (role === 'organizer') {
+            approved = false;
+        } 
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            role,
+            institution,
+            organizerRole: role === 'organizer' ? organizerRole : null,
+            organization: role === 'organizer' ? organization : null,
+            approved
+        })
+
+        await newUser.save();
+        return res.status(201).json({ message: approved ? 'Registered successfully' : 'Account pending approval' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Server error' });
     }
 }
