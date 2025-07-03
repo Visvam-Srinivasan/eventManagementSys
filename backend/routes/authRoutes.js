23a64b8dc9023e08b05bc5bcdb416f65d2311404
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-03 15:21:56 +0530

    feat: Add register routing

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const { loginUser } = '../controllers/authController.js';
+const { registerUser } = require('../controllers/authController');
 
 router.post('/login', loginUser);
+router.post('/register', registerUser)
 
 module.exports = router;
