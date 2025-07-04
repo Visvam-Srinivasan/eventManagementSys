cc1fd7ddc392e327a70cc4908773d1316769fab2
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-04 21:57:47 +0530

    feat: Add approve user route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { protect, restrictTo, requireApproval } = require('../middleware/authMiddleware');
-const { loginUser } = '../controllers/authController.js';
-const { registerUser } = require('../controllers/authController');
+const { loginUser, registerUser, approveUser } = '../controllers/authController.js';
 
 router.post('/login', loginUser);
 router.post('/register', registerUser)
+
+router.put(
+    '/approve/:id',
+    protect,
+    requireApproval,
+    approveUser
+);
 
 module.exports = router;
