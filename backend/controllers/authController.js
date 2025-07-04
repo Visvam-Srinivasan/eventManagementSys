75c8106c2e7597d97b00f2c7f838612e11158e97
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-04 21:55:50 +0530

    feat: Add approval function for both admin and organization head

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -57,3 +57,46 @@ exports.registerUser = async (req, res) => {
         return res.status(500).json({ message: 'Server error' });
     }
 }
+
+exports.approveUser = async (req, res) => {
+    const approver = req.user;
+    const { id } = req.params;
+
+    try {
+        const userToApprove = await User.findById(id);
+        if (!userToApprove) return res.status(404).json({ message: 'User not found'} );
+
+        //Admin approving Head of Organization
+        if (approver.role === 'admin') {
+            if (userToApprove.role === 'organizer' && userToApprove.organizerRole === 'head') {
+                userToApprove.approved = true;
+                await userToApprove.save();
+                return res.status(200).json({ message: 'Head organizer approved by admin.' });
+            } else {
+                return res.status(403).json({ message: 'Admin can only approve head of organization' });
+            }
+        }
+
+        //Head of organization approving Members
+        if (
+            approver.role === 'organizer' &&
+            approver.organizerRole === 'head'
+        ) {
+            if (
+                userToApprove.role === 'organizer' &&
+                userToApprove.organizerRole === 'member' &&
+                String(userToApprove.organization) === String(approver.organization)
+            ) {
+                userToApprove.approved = true;
+                await userToApprove.save();
+                return res.status(200).json({ message: 'Member approved by head' });
+            } else {
+                return res.status(403).json({ message: 'Head can only approve members' });
+            }
+
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Server error.' });
+    }
+}
