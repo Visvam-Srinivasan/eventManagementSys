d0485448ec7ed0e815754eb8b2aa54acf65a1883
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-05 13:47:53 +0530

    feat: Add getOrganizationMembers function

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -9,3 +9,13 @@ exports.getOrganizationHeads = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 }
+
+exports.getOrganizationMembers = async (req, res) => {
+    try {
+        const heads = await User.find({ role: 'organizer', organizerRole: 'member' }).populate('organization', 'name');
+        res.json(head);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
