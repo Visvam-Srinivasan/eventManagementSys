22b8fc3a929a48fc218c9e5a096c7c831c4816ae
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-05 13:49:37 +0530

    feat: Add getParticipants function

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -12,8 +12,18 @@ exports.getOrganizationHeads = async (req, res) => {
 
 exports.getOrganizationMembers = async (req, res) => {
     try {
-        const heads = await User.find({ role: 'organizer', organizerRole: 'member' }).populate('organization', 'name');
-        res.json(head);
+        const members = await User.find({ role: 'organizer', organizerRole: 'member' }).populate('organization', 'name');
+        res.json(members);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+exports.getParticipants = async (req, res) => {
+    try {
+        const participants = await User.find({ role: 'participant'});
+        res.json(participants);
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
