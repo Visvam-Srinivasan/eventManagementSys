bbc010fb1733900a5a1ccd6a4360acdcfbc25f3a
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-05 15:56:50 +0530

    feat: Add getUserById function

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -51,3 +51,14 @@ exports.getParticipants = async (req, res) => {
     }
 }
 
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate('organization', 'name');
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Server error" });
+    }
+}
