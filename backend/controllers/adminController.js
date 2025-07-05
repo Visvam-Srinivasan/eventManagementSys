12f59bfd98dc3d3ebbdb619cc10abc2b2bf647f4
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-07-05 15:54:23 +0530

    feat: Add pagination to getOrganizationHeads, getOrganizationMembers, getParticipants

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,9 +1,17 @@
+const getPagination = require('../utils/pagination');
 const User = require('../models/User');
 
 exports.getOrganizationHeads = async (req, res) => {
     try {
-        const heads = await User.find({ role: 'organizer', organizerRole: 'head' }).populate('organization', 'name');
-        res.json(head);
+        const heads = await User.find({ role: 'organizer', organizerRole: 'head' })
+            .select('_id name email approved organization')
+            .populate('organization', 'name')
+            .skip(skip)
+            .limit(limit);
+
+        const total = await User.countDocuments({ role: 'organizer', organizerRole: 'head'});
+
+        res.json({ total, page, limit, data: heads});
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
@@ -12,8 +20,15 @@ exports.getOrganizationHeads = async (req, res) => {
 
 exports.getOrganizationMembers = async (req, res) => {
     try {
-        const members = await User.find({ role: 'organizer', organizerRole: 'member' }).populate('organization', 'name');
-        res.json(members);
+        const members = await User.find({ role: 'organizer', organizerRole: 'member' })
+            .select('_id name email approved organization')
+            .populate('organization', 'name')
+            .skip(skip)
+            .limit(limit);
+
+        const total = await User.countDocuments({ role: 'organizer', organizerRole: 'member'});
+
+        res.json({ total, page, limit, data: members});
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
@@ -22,10 +37,17 @@ exports.getOrganizationMembers = async (req, res) => {
 
 exports.getParticipants = async (req, res) => {
     try {
-        const participants = await User.find({ role: 'participant'});
-        res.json(participants);
+        const participants = await User.find({ role: 'participant'})
+            .select('_id name email approved')
+            .skip(skip)
+            .limit(limit);
+
+        const total = await User.countDocuments({ role: 'participant'});
+        
+        res.json({ total, page, limit, data: participants});
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
     }
 }
+
